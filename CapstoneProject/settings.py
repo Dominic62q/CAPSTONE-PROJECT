a349d2895818f539bd229a53e4a2895b82443124
Dominic62q <dominicquainoo62@gmail.com>
2025-12-19 09:48:19 +0000

    Ready for Deployment

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f2nonprf&k4m70__g8#rs8h*mw-=#=@*dvcnt%&ivgk2fo&9(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
